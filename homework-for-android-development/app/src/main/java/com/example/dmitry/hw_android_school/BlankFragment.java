567a0d93c1b951f91b018b07fbf28fd1b723271a
dmitrytyunkov <tyunkov.dima@gmail.com>
2016-02-02 18:36:30 +0600

    Android project. In Activity added Blank Fragment in container.

--- homework-for-android-development/app/src/main/java/com/example/dmitry/hw_android_school/BlankFragment.java
+++ homework-for-android-development/app/src/main/java/com/example/dmitry/hw_android_school/BlankFragment.java
@@ -2,7 +2,7 @@ package com.example.dmitry.hw_android_school;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
