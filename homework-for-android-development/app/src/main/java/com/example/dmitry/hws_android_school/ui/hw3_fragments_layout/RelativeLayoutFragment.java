e1f1541d144cc563503ff38b5ba59a970c53c8b9
dmitrytyunkov <tyunkov.dima@gmail.com>
2016-02-25 14:44:09 +0600

    Created LinearLayout in HW3. Edited all layout.

---
+++ homework-for-android-development/app/src/main/java/com/example/dmitry/hws_android_school/ui/hw3_fragments_layout/RelativeLayoutFragment.java
@@ -0,0 +1,32 @@
+package com.example.dmitry.hws_android_school.ui.hw3_fragments_layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dmitry.hws_android_school.R;
+import com.example.dmitry.hws_android_school.ui.base.BaseFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RelativeLayoutFragment extends BaseFragment {
+
+
+    public RelativeLayoutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_relative_layout, container, false);
+    }
+
+
+}
