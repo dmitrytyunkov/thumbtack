334993a7cbd321025ca2900086ea048136044d83
dmitrytyunkov <tyunkov.dima@gmail.com>
2016-02-25 23:55:15 +0600

    Created handler for submit_button in LinearLayout.

--- homework-for-android-development/app/src/main/java/com/example/dmitry/hws_android_school/ui/hw3_fragments_layout/LinearLayoutFragment.java
+++ homework-for-android-development/app/src/main/java/com/example/dmitry/hws_android_school/ui/hw3_fragments_layout/LinearLayoutFragment.java
@@ -6,15 +6,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.dmitry.hws_android_school.R;
 import com.example.dmitry.hws_android_school.ui.base.BaseFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LinearLayoutFragment extends BaseFragment {
 
+    public static final String CHATMAIN = "chat_main";
+
+    @Bind(R.id.new_message_text)
+    EditText newMessageText;
+    @Bind(R.id.submit_button)
+    Button submitButton;
+    @Bind(R.id.linear_main)
+    LinearLayout linearMain;
 
     public LinearLayoutFragment() {
         // Required empty public constructor
@@ -25,8 +41,22 @@ public class LinearLayoutFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_linear_layout, container, false);
+        View view = inflater.inflate(R.layout.fragment_linear_layout, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 
 
+    @OnClick(R.id.submit_button)
+    public void onSubmitButtonClick() {
+        TextView textView = new TextView(getActivity());
+        textView.setText(newMessageText.getText());
+        linearMain.addView(textView);
+    }
 }
